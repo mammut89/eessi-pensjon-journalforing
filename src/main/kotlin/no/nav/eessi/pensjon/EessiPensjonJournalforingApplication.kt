7fbe8fa748cc92a034871dd8e6651443c8702a9e
Mo Amini <moh_amini@hotmail.com>
2021-01-03 21:48:36 +0100

    Update EessiPensjonJournalforingApplication.kt

--- src/main/kotlin/no/nav/eessi/pensjon/EessiPensjonJournalforingApplication.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/EessiPensjonJournalforingApplication.kt
@@ -3,12 +3,21 @@ package no.nav.eessi.pensjon
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.retry.annotation.EnableRetry
+import software.amazon.codeguruprofilerjavaagent.Profiler
+
 
 @SpringBootApplication
 @EnableRetry
 class EessiPensjonJournalforingApplication
 
 fun main(args: Array<String>) {
+	
+	Profiler.builder()
+        	.profilingGroupName("MyProfilingGroup")
+        	.build()
+        	.start()
+	
 	runApplication<EessiPensjonJournalforingApplication>(*args)
 }
+
 
